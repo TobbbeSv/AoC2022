feb1f2b1df2dc66a3bd698bf496da8478f1d5c5e
Toby <tobbbe@gmail.com>
2022-12-04 08:56:53 +0100

    setting up day 2

---
+++ day_2/day_2.py
@@ -0,0 +1,4 @@
+
+def day_2_part_1():
+    print("testing")
+    return 1
--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 from day_1.day_1 import day_1_part_1, day_1_part_2
+from day_2.day_2 import day_2_part_1
 
 
 def get_solution_for_day(solution_for_day: float):
-    switcher = {
-        1.1: day_1_part_1(),
-        1.2: day_1_part_2(),
-    }
-    return switcher.get(solution_for_day, None)
+    if solution_for_day == 1.1:
+        return day_1_part_1()
+    elif solution_for_day == 1.2:
+        return day_1_part_2()
+    elif solution_for_day == 2.1:
+        return day_2_part_1()
 
 
 if __name__ == "__main__":
-    day = 1.2
+    day = 2.1
     print(f"Executing day {day}")
     solution = get_solution_for_day(day)
     print(f"The solution is {solution}")
