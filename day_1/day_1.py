a25fb3a247ded5d36f92458d9cd34ac70c65d06a
Toby <tobbbe@gmail.com>
2022-12-04 08:35:10 +0100

    day 1 part 2

--- day_1/day_1.py
+++ day_1/day_1.py
@@ -2,6 +2,25 @@
 from elf_controller import retrieve_calorie_for_elves
 
 
-def day_1():
+def day_1_part_1():
     elves_with_calories = retrieve_calorie_for_elves()
     return max(elves_with_calories)
+
+
+def day_1_part_2():
+    elves_with_calories = retrieve_calorie_for_elves()
+
+    top_elf = max(elves_with_calories)
+    print(f"Top elf {top_elf}")
+
+    elves_with_calories.remove(top_elf)
+
+    second_elf = max(elves_with_calories)
+    print(f"Second elf {second_elf}")
+    elves_with_calories.remove(second_elf)
+
+    third_elf = max(elves_with_calories)
+    print(f"Third elf {third_elf}")
+    elves_with_calories.remove(third_elf)
+
+    return top_elf + second_elf + third_elf
--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-from day_1.day_1 import day_1
+from day_1.day_1 import day_1_part_1, day_1_part_2
 
 
-def get_solution_for_day(solution_for_day: int):
+def get_solution_for_day(solution_for_day: float):
     switcher = {
-        1: day_1()
+        1.1: day_1_part_1(),
+        1.2: day_1_part_2(),
     }
     return switcher.get(solution_for_day, None)
 
 
 if __name__ == "__main__":
-    day = 1
+    day = 1.2
     print(f"Executing day {day}")
     solution = get_solution_for_day(day)
-    print(f"The solution is {day_1()}")
+    print(f"The solution is {solution}")
