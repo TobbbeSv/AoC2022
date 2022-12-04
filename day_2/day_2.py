9dcfb74e6b935a4ce78e8ec57909304fd035675b
Toby <tobbbe@gmail.com>
2022-12-04 15:37:59 +0100

    Day 2 part 2

--- day_2/day_2.py
+++ day_2/day_2.py
@@ -10,9 +10,13 @@ LOOSE_POINT = 0
 DRAW_POINT = 3
 WIN_POINT = 6
 
+ROCK = ["A", "X"]
+PAPER = ["B", "Y"]
+SCISSOR = ["C", "Z"]
+
 
 def day_2_part_1():
-    print("day 2 part 2")
+    print("day 2 part 1")
     with open(os.path.join(sys.path[0], 'day_2/input.txt')) as games:
         points = 0
 
@@ -29,11 +33,28 @@ def day_2_part_1():
         return points
 
 
+def day_2_part_2():
+    print("day 2 part 2")
+    with open(os.path.join(sys.path[0], 'day_2/input.txt')) as games:
+        points = 0
+
+        for game in games:
+            game_points = 0
+            opponent_attack = get_attack_for_strategy(game[0])
+            needed_outcome = get_outcome_for_strategy(game[2])
+            player_attack = get_attack_for_outcome(opponent_attack, needed_outcome)
+            game_points += get_point_for_attack(player_attack)
+            game_points += get_match_point(opponent_attack, player_attack)
+            points += game_points
+
+        return points
+
+
 def get_attack_for_strategy(strategy):
     print(f"Strategy {strategy}")
-    if strategy == "A" or strategy == "X":
+    if strategy in ROCK:
         return Attack.Rock
-    elif strategy == "B" or strategy == "Y":
+    elif strategy in PAPER:
         return Attack.Paper
     else:
         return Attack.Scissor
@@ -53,24 +74,58 @@ def get_match_point(opponent_attack, player_attack):
     if player_attack == opponent_attack:
         return DRAW_POINT
 
-    if player_attack == Attack.Rock:
-        if opponent_attack == Attack.Paper:
+    if player_attack is Attack.Rock:
+        if opponent_attack is Attack.Paper:
             return LOOSE_POINT
         else:
             return WIN_POINT
-    elif player_attack == Attack.Paper:
-        if opponent_attack == Attack.Scissor:
+    elif player_attack is Attack.Paper:
+        if opponent_attack is Attack.Scissor:
             return LOOSE_POINT
         else:
             return WIN_POINT
     else:
-        if opponent_attack == Attack.Rock:
+        if opponent_attack is Attack.Rock:
             return LOOSE_POINT
         else:
             return WIN_POINT
+
+
+def get_outcome_for_strategy(strategy):
+    if strategy == "X":
+        return Outcome.LOOSE
+    elif strategy == "Y":
+        return Outcome.DRAW
+    else:
+        return Outcome.WIN
+
+
+def get_attack_for_outcome(opponent_attack, needed_outcome):
+    if needed_outcome is Outcome.DRAW:
+        return opponent_attack
+    elif opponent_attack is Attack.Rock:
+        if needed_outcome is Outcome.WIN:
+            return Attack.Paper
+        else:
+            return Attack.Scissor
+    elif opponent_attack is Attack.Scissor:
+        if needed_outcome is Outcome.WIN:
+            return Attack.Rock
+        else:
+            return Attack.Paper
+    elif needed_outcome is Outcome.WIN:
+        return Attack.Scissor
+    else:
+        return Attack.Rock
 
 
 class Attack(Enum):
     Rock = 1
     Paper = 2
     Scissor = 3
+
+
+class Outcome(Enum):
+    WIN = 1
+    LOOSE = 2
+    DRAW = 3
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from day_1.day_1 import day_1_part_1, day_1_part_2
-from day_2.day_2 import day_2_part_1
+from day_2.day_2 import day_2_part_1, day_2_part_2
 
 
 def get_solution_for_day(solution_for_day: float):
@@ -9,10 +9,12 @@ def get_solution_for_day(solution_for_day: float):
         return day_1_part_2()
     elif solution_for_day == 2.1:
         return day_2_part_1()
+    elif solution_for_day == 2.2:
+        return day_2_part_2()
 
 
 if __name__ == "__main__":
-    day = 2.1
+    day = 2.2
     print(f"Executing day {day}")
     solution = get_solution_for_day(day)
     print(f"The solution is {solution}")
