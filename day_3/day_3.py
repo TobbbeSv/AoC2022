923f2298eeaa0b234e6c66c64c49e2560c07dd0b
Toby <tobbbe@gmail.com>
2022-12-05 10:42:09 +0100

    Day 3 part 2

--- day_3/day_3.py
+++ day_3/day_3.py
@@ -6,7 +6,7 @@ UPPER_CASE_ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 
 def day_3_part_1():
-    print("day 2 part 1")
+    print("day 3 part 1")
     with open(os.path.join(sys.path[0], 'day_3/input.txt')) as rucksacks:
         priority_sum = 0
         for rucksack in rucksacks:
@@ -21,17 +21,40 @@ def day_3_part_1():
     return priority_sum
 
 
-def get_priority_sum_for_rucksack(first_compartment, second_compartment):
-    for item in first_compartment:
-        if item in second_compartment:
-            return get_priority_for_letter(item)
+def day_3_part_2():
+    print("day 3 part 2")
+    group_size = 3
+    with open(os.path.join(sys.path[0], 'day_3/input.txt')) as rucksacks:
+        rucksacks = rucksacks.readlines()
+        groups_amount = int(len(rucksacks) / group_size)
+        prioritized_items = 0
+        for index in range(groups_amount):
+            prioritized_items += check_elf_group_badges(group_size, index, rucksacks)
+
+        return prioritized_items
+
+
+def check_elf_group_badges(group_size, index, rucksacks):
+    group_leader = index * group_size
+    group = rucksacks[group_leader:group_leader + group_size]
+    for item in group[0]:
+        if item in group[1] and item in group[2]:
+            return get_priority_for_item(item)
     return 0
 
 
-def get_priority_for_letter(letter):
-    alphabet = UPPER_CASE_ALPHABET if letter.isupper() else LOWER_CASE_ALPHABET
+def get_priority_sum_for_rucksack(first_compartment, second_compartment):
+    for item in first_compartment:
+        if item in second_compartment:
+            return get_priority_for_item(item)
+    return 0
+
+
+def get_priority_for_item(item):
+    print(f"getting prioriry for {item}")
+    alphabet = UPPER_CASE_ALPHABET if item.isupper() else LOWER_CASE_ALPHABET
     start_index = 1
-    suffix_index = len(LOWER_CASE_ALPHABET) if letter.isupper() else 0
-    priority = alphabet.index(letter) + start_index + suffix_index
-    print(f"priority for item {letter} is {priority}")
+    suffix_index = len(LOWER_CASE_ALPHABET) if item.isupper() else 0
+    priority = alphabet.index(item) + start_index + suffix_index
+    print(f"priority for item {item} is {priority}")
     return priority
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from day_1.day_1 import day_1_part_1, day_1_part_2
 from day_2.day_2 import day_2_part_1, day_2_part_2
-from day_3.day_3 import day_3_part_1
+from day_3.day_3 import day_3_part_1, day_3_part_2
 
 
 def get_solution_for_day(solution_for_day: float):
@@ -14,10 +14,12 @@ def get_solution_for_day(solution_for_day: float):
         return day_2_part_2()
     elif solution_for_day == 3.1:
         return day_3_part_1()
+    elif solution_for_day == 3.2:
+        return day_3_part_2()
 
 
 if __name__ == "__main__":
-    day = 3.1
+    day = 3.2
     print(f"Executing day {day}")
     solution = get_solution_for_day(day)
     print(f"The solution is {solution}")
