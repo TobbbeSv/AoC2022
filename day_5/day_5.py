0598cec1537d5d1ebca536f5237116f9844e5e8f
Toby <tobbbe@gmail.com>
2022-12-06 15:08:22 +0100

    day 5 part 1

---
+++ day_5/day_5.py
@@ -0,0 +1,62 @@
+import os
+import sys
+import re
+
+CREATE_SIZE = 4
+INSTRUCTION_AMOUNT_OF_CREATES = 0
+INSTRUCTION_FROM_COLUMN = 1
+INSTRUCTION_TO_COLUMN = 2
+
+
+def day_5_part_1():
+    print("day 4 part 1")
+    with open(os.path.join(sys.path[0], 'day_5/input.txt')) as rows:
+        creates = []
+        instructions = []
+        extract_create_and_instructions(creates, instructions, rows)
+
+        print("Columns before rearrangement")
+        for create in creates:
+            print(create)
+
+        for index, instruction in enumerate(instructions):
+            move_creates_according_to_instructions(creates, instruction)
+            print("Columns during rearrangement")
+            for create in creates:
+                print(create)
+
+        top_creates = ""
+        for create in creates:
+            top_creates += create[-1][1]
+
+    return top_creates
+
+
+def move_creates_according_to_instructions(creates, instruction):
+    amount_of_creates = int(instruction[INSTRUCTION_AMOUNT_OF_CREATES])
+    from_column = creates[int(instruction[INSTRUCTION_FROM_COLUMN]) - 1]
+    to_column = creates[int(instruction[INSTRUCTION_TO_COLUMN]) - 1]
+    from_column_length = len(from_column)
+    index_divider = from_column_length - amount_of_creates
+    columns_to_move = from_column[index_divider: from_column_length]
+    creates[int(instruction[1]) - 1] = from_column[0: index_divider]
+    if isinstance(columns_to_move, list):
+        columns_to_move.reverse()
+    to_column.extend(columns_to_move)
+
+
+def extract_create_and_instructions(creates, instructions, rows):
+    for row in rows:
+        row = row.strip("\n")
+        if row.startswith("move"):
+            instructions.append(re.findall(r'\d+', row))
+        elif row != "" and row.strip().startswith("1") is False:
+            creates_for_row = [row[i:i + CREATE_SIZE] for i in range(0, len(row), CREATE_SIZE)]
+
+            if len(creates) == 0:
+                for _ in creates_for_row:
+                    creates.append([])
+            for index, create in enumerate(creates_for_row):
+                create = create.strip()
+                if create.strip() != "" and create != "---":
+                    creates[index].insert(0, create)
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from day_1.day_1 import day_1_part_1, day_1_part_2
 from day_2.day_2 import day_2_part_1, day_2_part_2
 from day_3.day_3 import day_3_part_1, day_3_part_2
 from day_4.day_4 import day_4_part_1, day_4_part_2
+from day_5.day_5 import day_5_part_1
 
 
 def get_solution_for_day(solution_for_day: float):
@@ -21,10 +22,12 @@ def get_solution_for_day(solution_for_day: float):
         return day_4_part_1()
     elif solution_for_day == 4.2:
         return day_4_part_2()
+    elif solution_for_day == 5.1:
+        return day_5_part_1()
 
 
 if __name__ == "__main__":
-    day = 4.2
+    day = 5.1
     print(f"Executing day {day}")
     solution = get_solution_for_day(day)
     print(f"The solution is {solution}")
