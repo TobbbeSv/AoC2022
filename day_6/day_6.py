3b4320d36681c32a5fa965c45ccc5281f73d367c
Toby <tobbbe@gmail.com>
2022-12-06 16:07:20 +0100

    day 6 part 2

--- day_6/day_6.py
+++ day_6/day_6.py
@@ -2,17 +2,19 @@ import os
 import sys
 
 SEQUENCE_MARKER_LENGTH = 4
+MESSAGE_MARKER_LENGTH = 14
 
 
-def day_6_part_1():
+def day_6(is_part_1: bool):
+    marker_length = SEQUENCE_MARKER_LENGTH if is_part_1 else MESSAGE_MARKER_LENGTH
     with open(os.path.join(sys.path[0], 'day_6/input.txt')) as sequence:
         sequence = sequence.readline().strip()
 
         for i in range(len(sequence)):
-            marker = sequence[i: i + SEQUENCE_MARKER_LENGTH]
+            marker = sequence[i: i + marker_length]
             print(f"Checking marker {marker}")
             print(f"Unique characters {len(dict.fromkeys(marker))}")
-            if len(dict.fromkeys(marker)) == 4:
+            if len(dict.fromkeys(marker)) == marker_length:
                 print("FOUND IT!")
-                return sequence.index(marker)+SEQUENCE_MARKER_LENGTH
+                return sequence.index(marker)+marker_length
     return 0
