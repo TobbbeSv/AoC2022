14fd4b3197ef9c45bc7c3224d5902d409cf6c05c
Toby <tobbbe@gmail.com>
2022-12-06 09:46:51 +0100

    Day 4 part 2

--- day_4/day_4.py
+++ day_4/day_4.py
@@ -3,21 +3,47 @@ import sys
 
 
 def day_4_part_1():
+    print("day 4 part 1")
     with open(os.path.join(sys.path[0], 'day_4/input.txt')) as cleaning_assignments:
         overlapping_assignments = 0
         for coupled_assignment in cleaning_assignments:
-            coupled_assignment = coupled_assignment.strip()
-            split_assignment = coupled_assignment.split(',')
-            first_assignment = split_assignment[0].split('-')
-            second_assignment = split_assignment[1].split('-')
-            first_range = range(int(first_assignment[0]), int(first_assignment[1]))
-            second_range = range(int(second_assignment[0]), int(second_assignment[1]))
+            first_range, second_range = get_ranges_from_assignments(coupled_assignment)
             print(f"Checking ranges {first_range} and {second_range}")
-            if is_range_in_range(first_range, second_range) or is_range_in_range(second_range, first_range):
+            if is_range_in_range(first_range, second_range):
+                overlapping_assignments += 1
+    return overlapping_assignments
+
+
+def day_4_part_2():
+    print("day 4 part 2")
+    with open(os.path.join(sys.path[0], 'day_4/input.txt')) as cleaning_assignments:
+        overlapping_assignments = 0
+        for coupled_assignment in cleaning_assignments:
+            first_range, second_range = get_ranges_from_assignments(coupled_assignment)
+            print(f"Checking ranges {first_range} and {second_range}")
+            if is_range_overlapping(first_range, second_range) or is_range_overlapping(second_range, first_range):
                 overlapping_assignments += 1
 
-    return overlapping_assignments
+        return overlapping_assignments
+
+
+def get_ranges_from_assignments(coupled_assignment):
+    coupled_assignment = coupled_assignment.strip()
+    split_assignment = coupled_assignment.split(',')
+    first_assignment = split_assignment[0].split('-')
+    second_assignment = split_assignment[1].split('-')
+    first_range = range(int(first_assignment[0]), int(first_assignment[1]))
+    second_range = range(int(second_assignment[0]), int(second_assignment[1]))
+    return first_range, second_range
 
 
 def is_range_in_range(first_range, second_range):
     return first_range.start >= second_range.start and first_range.stop <= second_range.stop
+
+
+def is_range_overlapping(first_range, second_range):
+    if is_range_in_range(first_range, second_range):
+        return True
+
+    return (first_range.start <= second_range.start <= first_range.stop) or (
+            second_range.start <= first_range.start <= second_range.stop)
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from day_1.day_1 import day_1_part_1, day_1_part_2
 from day_2.day_2 import day_2_part_1, day_2_part_2
 from day_3.day_3 import day_3_part_1, day_3_part_2
-from day_4.day_4 import day_4_part_1
+from day_4.day_4 import day_4_part_1, day_4_part_2
 
 
 def get_solution_for_day(solution_for_day: float):
@@ -19,10 +19,12 @@ def get_solution_for_day(solution_for_day: float):
         return day_3_part_2()
     elif solution_for_day == 4.1:
         return day_4_part_1()
+    elif solution_for_day == 4.2:
+        return day_4_part_2()
 
 
 if __name__ == "__main__":
-    day = 4.1
+    day = 4.2
     print(f"Executing day {day}")
     solution = get_solution_for_day(day)
     print(f"The solution is {solution}")
